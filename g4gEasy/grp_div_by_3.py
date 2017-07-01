a1ea9691537e297ddde2da04ae5740f293a49b9f
Shubham Gupta <shum979@gmail.com>
2017-07-01 14:26:50 +0530

    refactored--optimization

--- g4gEasy/grp_div_by_3.py
+++ g4gEasy/grp_div_by_3.py
@@ -1,26 +1,40 @@
 import sys
 
+#problem statement
+#http://practice.geeksforgeeks.org/problems/number-of-groups/0
 
-def solution(arr_str):
-    # arrStr = "3 6 9 12"
-    arr = [int(i) for i in arr_str.split(" ")]
-    cntr_2 = 0;
-    cntr_3 = 0;
+def ncr(n, r):
+    product = 1;
+    deno = 1;
+    for x in range(r):
+        product *= (n - x)
+        deno *= (x + 1)
+    return int(product / deno)
 
-    while arr:
-        first = arr.pop()
-        newArr = list(arr)
-        while newArr:
-            second = newArr.pop()
-            cntr_2 += 1 if ((first + second) % 3) == 0 else 0;
-            for x in newArr:
-                cntr_3 += 1 if ((first + second + x) % 3) == 0 else 0;
 
-    print(cntr_2 + cntr_3)
+def solution2(arr_str):
+    # arr_str = "3 6 9 12"
+    zeros = 0;
+    ones = 0;
+    twos = 0;
+
+    for i in arr_str.split(" ") :
+        if int(i) % 3 == 0 :
+            zeros +=1
+        elif int(i) % 3 == 1 :
+            ones += 1
+        else:
+            twos +=1
+
+    print(zeros,ones,twos)
+    count = ncr(zeros,2) + ones*twos + ncr(ones,3) + ncr(zeros,3) + ncr(twos,3) + zeros*ones*twos;
+    print(count)
 
 
 cases = int(sys.stdin.readline())
 for n in range(cases):
     size = int(sys.stdin.readline());
     arr_str = sys.stdin.readline().strip()
-    solution(arr_str)
+    solution2(arr_str)
+
+# solution2("1 5 7 2 9 14")
